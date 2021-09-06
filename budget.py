f18499b7e37b0895d4efa171803341a283d872ee
Brian Marquis <rbmarquis.dev@gmail.com>
2021-09-06 18:53:41 -0500

    define class methods

--- budget.py
+++ budget.py
@@ -1,40 +1,48 @@
 class Category:
+
     def __init__(self, category):
         self.category = category
         self.ledger = list()
 
     def deposit(self, amount, description = ''):
-        # should append an object to the ledger list in the form of {"amount": amount, "description": description}.
         self.ledger.append({"amount": amount, "description": description})
 
     def withdraw(self, amount, description = ''):
-        # amount passed in should be stored in the ledger as a negative number.
-        #  If there are not enough funds, nothing should be added to the ledger. 
-        return # This method should return True if the withdrawal took place, and False otherwise.
+        if self.check_funds(amount):
+            self.ledger.append({"amount": amount * -1, "description": description})
+            return True
+
+        return False
 
     def get_balance(self): 
-        return  # returns the current balance of the budget category based on the deposits and withdrawals that have occurred.
+        balance = 0
+        for entry in self.ledger:
+            balance += entry['amount']
 
-    def transfer(self, amount, category):
-        # should add a withdrawal with the amount and the description "Transfer to [Destination Budget Category]".
-        # should then add a deposit to the other budget category with the amount and the description "Transfer from [Source Budget Category]" 
-        # If there are not enough funds, nothing should be added to either ledgers. 
-        return # This method should return True if the transfer took place, and False otherwise.
+        return balance
+
+    def transfer(self, amount, target_category):
+        if self.check_funds(amount):
+            self.ledger.append({"amount": amount * -1, "description": f"Transfer to {target_category.category}"})
+            target_category.deposit(amount, f"Transfer from {self.category}")
+            return True
+
+        return False
 
     def check_funds(self, amount):
-        # This method should be used by both the withdraw method and transfer method.
-        return  # returns False if the amount is greater than the balance of the budget category and returns True otherwise. 
+        balance = self.get_balance()
+        return False if amount > balance else True
 
     def __str__(self):
-        # When the budget object is printed it should display:
-        # A title line of 30 characters where the name of the category is centered in a line of * characters. 
-        # A list of the items in the ledger. Each line should show the description and amount. The first 23 characters of the description should be displayed, then the amount. The amount should be right aligned, contain two decimal places, and display a maximum of 7 characters. 
-        # A line displaying the category total.
-        return 
+        print_str = f"{self.category:*^30}"
 
+        for entry in self.ledger: 
+            print_str += f"\n{entry['description']:<23.23}"
+            print_str += f"{entry['amount']:>7.2f}"
 
+        print_str += f"\nTotal: {self.get_balance()}"
 
-
+        return print_str
 
 def create_spend_chart(categories):
     return  # should return a string that is a bar chart.
@@ -48,3 +56,28 @@ def create_spend_chart(categories):
     # There should be a title at the top that says "Percentage spent by category".
     # This function will be tested with up to four categories.
 
+# Testing
+food = Category("Food")
+food.deposit(1000, "initial deposit")
+food.withdraw(10.15, "groceries")
+food.withdraw(15.89, "restaurant and more food for dessert")
+# print(food.get_balance()) # 973.96
+# print(food.ledger)
+clothing = Category("Clothing")
+food.transfer(50, clothing)
+# print(food.get_balance()) # 923.96
+# print(clothing.get_balance()) # 50
+# print(food.ledger)
+# print(clothing.ledger)
+clothing.withdraw(25.55) 
+# print(clothing.get_balance()) # 24.45
+clothing.withdraw(100)
+# print(clothing.get_balance()) # 24.45
+auto = Category("Auto")
+auto.deposit(1000, "initial deposit")
+auto.withdraw(15)
+# print(auto.get_balance()) # 985
+print(food)
+print(clothing)
+
+# print(create_spend_chart([food, clothing, auto]))
